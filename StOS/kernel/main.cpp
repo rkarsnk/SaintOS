e2d5f939e9df9875892104d0ad0d8379bebafedd
rkarsnk <git-repos-rkarsnk@rkarsnk.jp>
2021-05-05 15:52:52 +0900

    白塗りを黒塗りに変える.

--- StOS/kernel/main.cpp
+++ StOS/kernel/main.cpp
@@ -41,12 +41,7 @@ int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
     for(int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
         for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255});
-        }
-    }
-    for (int x = 0; x < 200; ++x) {
-        for (int y = 0; y < 100; ++y) {
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+            WritePixel(frame_buffer_config, x, y, {0, 0, 0});
         }
     }
     while (1) __asm__("hlt");
